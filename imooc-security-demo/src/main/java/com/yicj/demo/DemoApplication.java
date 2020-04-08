58fc15db174ecd8f56618b9aacbe1792af5a0a26
易成杰 <626659321@qq.com>
2020-04-08 10:24:35 +0800

    项目骨架搭建

--- imooc-security-demo/src/main/java/com/yicj/demo/DemoApplication.java
+++ imooc-security-demo/src/main/java/com/yicj/demo/DemoApplication.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+package com.yicj.demo;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class,args) ;
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello spring security" ;
+    }
 }
