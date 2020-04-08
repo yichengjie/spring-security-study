452f7b4b7561e01b3c8fbced81bc7764b799ed26
易成杰 <626659321@qq.com>
2020-04-08 17:37:29 +0800

    文件上传下载

--- imooc-security-demo/readme.txt
+++ imooc-security-demo/readme.txt
@@ -6,3 +6,4 @@
      请求头Accept中是否包含text/html判断是返回页面还是json数据
 5. restfule api的拦截
    过滤器、拦截器、切片(aspect)
+6. 使用切面时debug打在controller方法中的断点无法进入
---
+++ imooc-security-demo/src/main/java/com/yicj/dto/FileInfo.java
@@ -0,0 +1,18 @@
+package com.yicj.dto;
+public class FileInfo {
+	
+	public FileInfo(String path){
+		this.path = path;
+	}
+	
+	private String path;
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+}
